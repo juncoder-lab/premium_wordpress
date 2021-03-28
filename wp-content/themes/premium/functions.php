db20cb0c98effd65c2ce113beb6adf1abfc8f4b0
Sergey S <smirnof.sergey@gmail.com>
2021-03-28 15:21:26 +0300

    start woocommerce

--- wp-content/themes/premium/functions.php
+++ wp-content/themes/premium/functions.php
@@ -4,6 +4,10 @@ add_action('wp_enqueue_scripts', 'add_scripts');
 add_action('after_setup_theme', 'my_menu');
 add_filter('nav_menu_link_attributes', 'set_class_nav_menu_links',10,3);
 add_filter('nav_menu_link_attributes', 'set_class_nav_menu_links',10,3);
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
 function add_styles() {
 	wp_enqueue_style('bootstrap', get_template_directory_uri().'/assets/css/bootstrap.min.css');
 	wp_enqueue_style('main_style', get_stylesheet_uri());
